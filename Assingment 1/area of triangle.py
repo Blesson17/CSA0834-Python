2ddf4ab82c87ff4cc007d75a19671df9c7fad713
Blesson <108485446+Blesson17@users.noreply.github.com>
2024-08-30 12:21:55 +0530

    Add files via upload

---
+++ Assingment 1/area of triangle.py
@@ -0,0 +1,6 @@
+def area_triangle(base, height):
+  return 0.5 * base * height
+base = float(input("Enter the base length: "))
+height = float(input("Enter the height length: "))
+result = area_triangle(base, height)
+print("Area of the triangle:", result)
---
+++ Assingment 1/leap year.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the year : "))
+if a%4==0:
+   print("It is an leap year")
+else:
+  print("It is not a leap year")
+  
